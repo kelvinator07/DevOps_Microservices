c59f49665d2eead718982790906927c2332344cd
Kelvin Isievwore <kelvinator4leo@gmail.com>
2021-10-20 01:40:56 +0100

    Updated Dockerfile, Makefile, config.yml

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,17 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+# docker build -t projectml .
+docker build --tag=projectml:latest .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+# docker run -d -p 8000:80 projectml:latest
+docker run -p 8000:80 projectml:latest
+
+# debug
+# docker run -it --entrypoint /bin/bash projectml 
